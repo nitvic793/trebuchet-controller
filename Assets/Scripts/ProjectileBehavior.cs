645f4f236c371a9e863c0c8f48f2581bca62ec52
Nitish Victor <nithishvictor@gmail.com>
2018-03-27 13:06:59 -0400

    Initial commit

---
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    SwingArm armData;
+
+    float armRotation;
+    float baseRotation;
+    Vector3 trebuchetDirection;
+    Rigidbody projectileRigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        var beam = GameObject.Find("Beam");
+        armData = beam.GetComponent<SwingArm>();
+        trebuchetDirection = armData.trebuchetDirection;
+        armRotation = armData.armRotation - 138;
+        baseRotation = armData.trebuchetRotation;
+        projectileRigidBody = GetComponent<Rigidbody>();
+        var actualForce = Normalize(armRotation, 120, 0);
+        var totalForce = new Vector3(-1000 * actualForce, 300 * actualForce, 0);
+        totalForce = Quaternion.Euler(0, baseRotation, 0) * totalForce;
+        projectileRigidBody.AddForce(totalForce);
+    }
+
+    float Normalize(float value, float max, float min)
+    {
+        var normalized = (value - min) / (max - min);
+        return normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
---
+++ Assets/Scripts/SwingArm.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+
+public class SwingArm : MonoBehaviour
+{
+    public float speed;
+
+    private float amountToMove;
+
+    public GameObject beamArm;
+    public GameObject trebuchet;
+    public GameObject canonPrefab;
+
+    SerialPort serialPort = new SerialPort("COM3", 9600);
+
+    public float armRotation = 0F;
+    public float trebuchetRotation = 0F;
+    public Vector3 trebuchetDirection;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        serialPort.Open();
+        StartCoroutine(ReadDataFromSerialPort());
+    }
+
+    IEnumerator ReadDataFromSerialPort()
+    {
+        // Debug.Log("Read values from ARDUINO UNO");
+        while (true)
+        {
+            Debug.Log("Arduino initated");
+            string[] values = serialPort.ReadLine().Split(',');
+            armRotation = (float.Parse(values[0]));
+            trebuchetRotation = (float.Parse(values[1]));
+
+
+            Debug.Log("direction: " + armRotation);
+            Debug.Log("trebuchetRotation: " + trebuchetRotation);
+            Debug.Log("***********************************************************");
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        amountToMove = speed * Time.deltaTime;
+        beamArm.transform.rotation = Quaternion.Euler(0, trebuchetRotation, armRotation);
+        trebuchet.transform.rotation = Quaternion.Euler(0, trebuchetRotation, 0);
+        trebuchetDirection = transform.forward;
+        // beamArm.transform.setr(0f, 0f, direction);
+        //trebuchet.transform.Rotate(0f, trebuchetRotation, 0f);
+
+
+
+
+        //if (!serialPort.IsOpen)
+        //    serialPort.Open();
+
+
+        //  string[] values = serialPort.ReadLine().Split(',');
+        //direction = (float.Parse(values[0])) / 100;
+        //Debug.Log("direction: " + direction);
+        var instantiatePosition = beamArm.transform.position;
+        instantiatePosition.x -= 2;
+        GameObject canon;
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            canon = Instantiate(canonPrefab, instantiatePosition, beamArm.transform.rotation, transform.parent) ;
+        }
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        serialPort.Close();
+    }
+
+}
