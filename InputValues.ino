9cae27bb07958199d538dbac2057260d7c78ec60
Neevan09 <36045501+Neevan09@users.noreply.github.com>
2018-03-28 08:14:25 -0400

    Arduino Controller Input Values

---
+++ InputValues.ino
@@ -0,0 +1,26 @@
+int beamRotation = 0;
+int trebuchetRotation = 0;
+
+void setup() 
+{
+  Serial.begin(9600);  
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+}
+
+void loop()
+{
+  beamRotation = analogRead(A0);
+  trebuchetRotation = analogRead(A1);
+  Serial.print(map(beamRotation,0,1023,0,270)); 
+
+  Serial.print(",");
+  Serial.println(map(trebuchetRotation,0,1023,0,270));  
+  
+  delay(50);   
+}
+
+
+
+
+
